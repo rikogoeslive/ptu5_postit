906909ad0b92ec9535903d9e9cd8a59478620972
Riko <mntsklp@gmail.com>
2022-11-25 13:27:35 +0200

    Serializer comments in post

--- ptu5_postit/postit_api/serializers.py
+++ ptu5_postit/postit_api/serializers.py
@@ -2,14 +2,6 @@ from rest_framework import serializers
 from . import models
 
 
-class PostSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
-    user_id = serializers.ReadOnlyField(source='user.id')
-
-    class Meta:
-        model = models.Post
-        fields = ('id', 'title', 'body', 'user', 'user_id', 'created_at')
-
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     user_id = serializers.ReadOnlyField(source='user.id')
@@ -18,3 +10,12 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Comment
         fields = ('id', 'post', 'body', 'user', 'user_id', 'created_at')
+
+class PostSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+    comments = CommentSerializer(many=True)
+
+    class Meta:
+        model = models.Post
+        fields = ('id', 'title', 'body', 'user', 'user_id', 'created_at', 'comments' )

b5b3e92e1298575bd8247af7c7f400bae95753f6
Riko <mntsklp@gmail.com>
2022-11-28 09:04:35 +0200

    added comment count

--- ptu5_postit/postit_api/serializers.py
+++ ptu5_postit/postit_api/serializers.py
@@ -14,8 +14,14 @@ class CommentSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     user_id = serializers.ReadOnlyField(source='user.id')
+    comments_count = serializers.SerializerMethodField()
     comments = CommentSerializer(many=True)
+    #comments = serializers.StringRelatedField(many=True) (nenaudoti)  
+
+    def get_comments_count(self, obj):
+        return models.Comment.objects.filter(post=obj).count()
+
 
     class Meta:
         model = models.Post
-        fields = ('id', 'title', 'body', 'user', 'user_id', 'created_at', 'comments' )
+        fields = ('id', 'title', 'body', 'user', 'user_id', 'created_at', 'comments_count', 'comments')
